60fcb5a3591ea3f3417eaedab75f76fdc26330c4
Edafe Akpokiniovo <Edafe.Akpokiniovo@Tibell2020outlook.onmicrosoft.com>
2024-07-05 15:06:09 +0100

    another commit

--- app.js
+++ app.js
@@ -10,13 +10,13 @@ app.use(express.urlencoded({extended: true}))
 app.get('/api/hello', async(req,res)=>{
     const response = await axios.get(`https://api.ip2location.io/?key=${process.env.API_KEY1}&ip=${req.ip}`, { method: 'POST', body: 'a=1' });
     const data = response.data
-    const response2 = await axios.get(`https://api.weatherapi.com/v1/current.json?key=${process.env.API_KEY2}&q=${data.city}`);
+    // const response2 = await axios.get(`https://api.weatherapi.com/v1/current.json?key=${process.env.API_KEY2}&q=${data.city}`);
     const params = req.query
     console.log(response2.data)
     res.json({
         "client_ip":data.ip,
         "location": data.city,
-        "greeting": `Hello,${params.visitor_name}, the temperature is ${response.data.current.temp_c}degrees Celcius in ${data.city}`,
+        "greeting": `Hello,${params.visitor_name}, the temperature is 15 degrees Celcius in ${data.city}`,
         
     })
 })
